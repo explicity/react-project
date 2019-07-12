037024eb8e780644f4eb2a6922d30df9991df5ba
Olya Yakymovych <msolya@outlook.com>
2019-07-12 15:39:39 +0300

    set up react + webpack + babel + eslint

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'index.bundle.js'
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    }),
+    new BaseHrefWebpackPlugin({ baseHref: '/' })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    host: 'localhost',
+    port,
+    historyApiFallback: true,
+    open: true,
+    hot: true
+  }
+};
