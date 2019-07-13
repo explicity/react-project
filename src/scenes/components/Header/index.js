c921ab342601b15142658e75d5bffe448ecc2bb7
Olya Yakymovych <msolya@outlook.com>
2019-07-13 19:44:43 +0300

    add header component

---
+++ src/scenes/components/Header/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavLink
+} from 'reactstrap';
+
+import './header.scss';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+  }
+
+  render() {
+    const { participants, messagesAmount, lastMessage } = this.props.data;
+    const { isOpen } = this.state;
+
+    return (
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">My chat</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink>
+                {participants} participant
+                {participants !== 1 && 's'}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                {messagesAmount} message
+                {messagesAmount !== 1 && 's'}
+              </NavLink>
+            </NavItem>
+            <NavItem className="time">
+              <p>last message at {lastMessage}</p>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
