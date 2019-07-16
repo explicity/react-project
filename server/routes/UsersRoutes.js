9b3060f816d4a4e5f87a98c79bb0d5db53e17906
Olya Yakymovych <msolya@outlook.com>
2019-07-16 21:14:17 +0300

    create user routes

---
+++ server/routes/UsersRoutes.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+
+const uuid = require('uuid');
+const Joi = require('@hapi/joi');
+
+const validateUser = (user) => {
+  const schema = {
+    id: Joi.string(),
+    username: Joi.string().min(2),
+    password: Joi.string(),
+    role: Joi.string(),
+    email: Joi.string()
+  };
+
+  return Joi.validate(user, schema);
+};
+
+const users = require('../data/users');
+
+router.get('/', (req, res, next) => {
+  return res.json(users);
+});
+
+router.get('/:id', (req, res, next) => {
+  const found = users.some(user => user.id === req.params.id);
+
+  if (found) {
+    res.json(users.filter(user => user.id === req.params.id));
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No member with the id of ${req.params.id} was found` });
+  }
+})
+
+router.post('/', (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;  
+  }
+
+  const newUser = {
+    id: uuid.v4(),
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  };
+
+  const { id, username, password, email } = newUser;
+
+  if (!id || !username || !password || !email) {
+    res.status(400).json({ msg: 'Please fill all the requiered fields.' });
+  }
+
+  users.push(newUser);
+  res.json(users);
+});
+
+router.put('/:id', (req, res) => {
+  const found = users.some(user => user.id === req.params.id);
+
+  if (found) {
+    const updUser = req.body;
+
+    const { error } = validateUser(updUser);
+    if (error) {
+      res.status(400).send(error.details[0].message);
+      return;
+    }
+
+    users.forEach((user) => {
+      let { id, username, password, email } = updUser;
+
+      if (user.id === req.params.id) {
+        user.username = username ? username : user.username;
+        user.password = password ? password : user.password;
+        user.email = email ? email : user.email;
+
+        res.json({ msg: 'Member update', user });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: 'Cannot update' });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const found = users.find(user => user.id === req.params.id);
+
+  if (found) {
+    const index = users.indexOf(found);
+    users.splice(index, 1);
+    res.json({
+      msg: 'User deleted',
+      users
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No member with the id of ${req.params.id} was found` });
+  }
+});
+
+module.exports = router;
